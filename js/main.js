ddb41a5555dd4613bf94df2ec83e7fffd961c7f5
Maire Cassandre <cassandre88@icloud.com>
2021-04-15 20:08:03 +0200

    ajout de l'ocean

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ function configureAssetManager(scene) {
 }
 
 function createSkybox(scene){
-	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:2000.0}, scene);
+	var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:2300.0}, scene);
     var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
     skyboxMaterial.backFaceCulling = false;
     skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/Skybox/TropicalSunnyDay", scene);
@@ -90,19 +90,39 @@ function createSkybox(scene){
 }
 
 function createGround(scene){
+	//On crée d'abord un sol "sabloneux" grâce à une texture et un bumb qui lui sont propres
 	var groundTexture = new BABYLON.Texture("assets/textures/sand.jpg", scene);
 	groundTexture.vScale = groundTexture.uScale = 4.0;
 	
 	var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
 	groundMaterial.diffuseTexture = groundTexture;
 	
-	var ground = BABYLON.Mesh.CreateGround("ground", 1000, 1000, 32, scene, false);
+	var ground = BABYLON.Mesh.CreateGround("ground", 2000, 2000, 32, scene, false);
 	ground.position.y = -1;
 	ground.material = groundMaterial;
+
+	// On crée ensuite un second sol qui est un Water Material, il représente la mer
+	// On lui donne des attributs qui nous permettent de créer des vagues avec un effet de vent
+	var waterMesh = BABYLON.Mesh.CreateGround("sea", 2000, 2000, 32, scene, false);
+	var water = new BABYLON.WaterMaterial("water", scene, new BABYLON.Vector2(1024, 1024));
+	water.backFaceCulling = true;
+	water.bumpTexture = new BABYLON.Texture("assets/textures/waterbump.png", scene);
+	water.windForce = -6;
+	water.windDirection = new BABYLON.Vector2(1, 1);
+	water.waveHeight = 1.7;
+	water.bumpHeight = 0.8;
+	water.waveLength = 1;
+	water.waterColor = new BABYLON.Color3(0.2, 0.2, 0.6);
+	water.colorBlendFactor = 0.2;
+	// On fait en sorte que nos vagues reflètent la skybox et le sol
+	let skybox = scene.getMeshByName("skyBox");
+	water.addToRenderList(skybox);
+	water.addToRenderList(ground);
+	waterMesh.material = water;
 }
 
 function createLights(scene) {
     // i.e sun light with all light rays parallels, the vector is the direction.
     let light0 = new BABYLON.DirectionalLight("dir0", new BABYLON.Vector3(-1, -1, 0), scene);
-    light0.intensity = 0.9;
+    light0.intensity = 1;
 }
