72ea0a0f14c099b1d4c28be2b02b24ec16dbccec
Maire Cassandre <cassandre88@icloud.com>
2021-04-16 14:32:33 +0200

    music

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function startGame() {
 
 	scene.enablePhysics();
   
-	//modifySettings();
+	modifySettings();
     
 	scene.toRender = () => {
 	  let deltaTime = engine.getDeltaTime();
@@ -36,7 +36,7 @@ function createScene() {
   
 	//createLights(scene);
 	createSkybox(scene);
-	//loadSounds(scene);
+	loadSounds(scene);
   
 	return scene;
 }
@@ -151,3 +151,148 @@ function createLights(scene) {
     let light0 = new BABYLON.DirectionalLight("dir0", new BABYLON.Vector3(-1, -1, 0), scene);
     light0.intensity = 1;
 }
+
+function loadSounds(scene) {
+	var assetsManager = scene.assetsManager;
+  
+	var binaryTask = assetsManager.addBinaryFileTask(
+	  "cannonSound",
+	  "assets/sounds/cannonBlast.mp3"
+	);
+	binaryTask.onSuccess = function (task) {
+	  scene.assets.cannonSound = new BABYLON.Sound(
+		"cannon",
+		task.data,
+		scene,
+		null,
+		{ loop: false, spatialSound: true }
+	  );
+	};
+
+	binaryTask = assetsManager.addBinaryFileTask(
+	  "explosion",
+	  "assets/sounds/explosion.mp3"
+	);
+
+	binaryTask.onSuccess = function (task) {
+	  scene.assets.explosion = new BABYLON.Sound(
+		"explosion",
+		task.data,
+		scene,
+		null,
+		{ loop: false, spatialSound: true }
+	  );
+	};
+  
+	binaryTask = assetsManager.addBinaryFileTask(
+	  "pirates",
+	  "assets/sounds/PiratesCaraiben.mp3"
+	);
+	binaryTask.onSuccess = function (task) {
+	  scene.assets.pirateMusic = new BABYLON.Sound(
+		"piratesCaraiben",
+		task.data,
+		scene,
+		null,
+		{
+		  loop: true,
+		  autoplay: true,
+		}
+	  );
+	};
+}
+
+window.addEventListener("resize", () => {
+	engine.resize();
+});
+  
+function modifySettings() {
+	// as soon as we click on the game window, the mouse pointer is "locked"
+	// you will have to press ESC to unlock it
+	scene.onPointerDown = () => {
+	  if (!scene.alreadyLocked) {
+		console.log("requesting pointer lock");
+		canvas.requestPointerLock();
+	  } else {
+		console.log("Pointer already locked");
+	  }
+	};
+  
+	document.addEventListener("pointerlockchange", () => {
+	  let element = document.pointerLockElement || null;
+	  if (element) {
+		// lets create a custom attribute
+		scene.alreadyLocked = true;
+	  } else {
+		scene.alreadyLocked = false;
+	  }
+	});
+  
+	//add the listener to the main, window object, and update the states
+	window.addEventListener(
+	  "keydown",
+	  (event) => {
+		if (event.key === "ArrowLeft" || event.key === "q" || event.key === "Q") {
+		  scene.inputStates.left = true;
+		} else if (
+		  event.key === "ArrowUp" ||
+		  event.key === "z" ||
+		  event.key === "Z"
+		) {
+		  scene.inputStates.up = true;
+		} else if (
+		  event.key === "ArrowRight" ||
+		  event.key === "d" ||
+		  event.key === "D"
+		) {
+		  scene.inputStates.right = true;
+		} else if (
+		  event.key === "ArrowDown" ||
+		  event.key === "s" ||
+		  event.key === "S"
+		) {
+		  scene.inputStates.down = true;
+		} else if (event.key === " ") {
+		  scene.inputStates.space = true;
+		} else if (event.key === "l" || event.key === "L") {
+		  scene.inputStates.laser = true;
+		} else if (event.key == "t" || event.key == "T") {
+		  scene.activeCamera = scene.followCameraTank;
+		} else if (event.key == "y" || event.key == "Y") {
+		  scene.activeCamera = scene.followCameraDude;
+		}
+	  },
+	  false
+	);
+  
+	//if the key will be released, change the states object
+	window.addEventListener(
+	  "keyup",
+	  (event) => {
+		if (event.key === "ArrowLeft" || event.key === "q" || event.key === "Q") {
+		  scene.inputStates.left = false;
+		} else if (
+		  event.key === "ArrowUp" ||
+		  event.key === "z" ||
+		  event.key === "Z"
+		) {
+		  scene.inputStates.up = false;
+		} else if (
+		  event.key === "ArrowRight" ||
+		  event.key === "d" ||
+		  event.key === "D"
+		) {
+		  scene.inputStates.right = false;
+		} else if (
+		  event.key === "ArrowDown" ||
+		  event.key === "s" ||
+		  event.key === "S"
+		) {
+		  scene.inputStates.down = false;
+		} else if (event.key === " ") {
+		  scene.inputStates.space = false;
+		}
+	  },
+	  false
+	);
+  }
